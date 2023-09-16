ceb55d0c8629fa907957089ab28d88cdb37ecea3
Programmieren-mit-Pascal <142749380+Programmieren-mit-Pascal@users.noreply.github.com>
2023-09-16 17:51:29 +0200

    Teil 4 des Tutorials.py

--- raycaster.py
+++ raycaster.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 pygame.init()
 
 # The world map is represented by a 2D-Array. 
@@ -24,14 +25,31 @@ world_map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
              [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
-WIN_WIDTH = 600
-WIN_HEIGHT = 600
+WIN_WIDTH = 900
+WIN_HEIGHT = 900
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Raycaster")
 clock = pygame.time.Clock()
 
 FPS = 60
 BLOCK_SIZE = WIN_HEIGHT / len(world_map)
+
+# Constants for player movement
+MOVE_SPEED = 0.06
+ROTATION_SPEED = math.radians(1.7)
+
+# Starting position and direction of player
+player_x = 1.5
+player_y = 5.2
+player_direction = math.radians(350)
+
+# We do not want the player to be on an exact integer position or have a direction of exactly 0.
+# Occurrences of these events could result in glitches or division-by-zero errors.
+# By adding a little starting offset to the values, we make
+# these events extremely unlikely (practically impossible) to occur.
+player_x += 0.0000001
+player_y += 0.0000001
+player_direction += 0.0000001
 
 run = True
 while run:
@@ -51,6 +69,11 @@ while run:
             color = (0, 200, 0) if world_map[row][column] == 1 else (200, 200, 200)
             pygame.draw.rect(screen, color, (block_x, block_y, BLOCK_SIZE, BLOCK_SIZE))
             pygame.draw.rect(screen, (100, 100, 100), (block_x, block_y, BLOCK_SIZE, BLOCK_SIZE), 2)
+            
+    # Draw player
+    player_screen_x = player_x * BLOCK_SIZE
+    player_screen_y = player_y * BLOCK_SIZE
+    pygame.draw.circle(screen, (255, 0, 0), (player_screen_x, player_screen_y), 10)
 
     pygame.display.flip()
             
